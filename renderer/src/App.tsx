3ed869d2ffb5eef60d4d4ebaa0c4b694ba668fc8
megagig <megagigsoftwaresolutions@gmail.com>
2026-03-08 16:19:21 +0100

    Setup Tailwind and Mantine Ui for styling

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@mantine/core"
+
 function App() {
   return (
-    <div>
-      <h1>Laundry Management System</h1>
-      <p>Welcome to the Desktop App</p>
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-10 rounded-xl shadow-lg space-y-4">
+        <h1 className="text-3xl font-bold">
+          Laundry Manager
+        </h1>
+
+        <Button color="blue">
+          Create Order
+        </Button>
+      </div>
     </div>
   )
 }
